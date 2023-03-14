685b50c2aa0a6d251dd859391782f363e453ea59
Caroline Tang <caroline.tang77@gmail.com>
2023-03-14 10:27:23 -0700

    added dropdown for season

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from dash import dash, html
+import altair as alt
+import pandas as pd
+from dash import dash, dcc, html, Input, Output
 
+# import data
+dr_data = pd.read_csv('data/drag.csv', index_col=0)
 app = dash.Dash(__name__)
 
-app.layout = html.Div('I am alive!!')
+app.layout = html.Div([
+    html.H1('Drag Race Visualizer'),
+    dcc.Dropdown(
+        id = 'season', 
+        options = sorted(dr_data['season'].unique()),
+        value = 'S01'
+    )
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
