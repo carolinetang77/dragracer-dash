6f83f51044dfea619d404f4f55e67e7f3fed700a
Caroline Tang <caroline.tang77@gmail.com>
2023-03-16 22:13:29 -0700

    updated layout and theme

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import plotly.express as px
 import pandas as pd
 import dash_bootstrap_components as dbc
-from dash import dash, dcc, html, Input, Output, State
+from dash import dash, dcc, dash_table, html, Input, Output, State
+from dash_bootstrap_templates import load_figure_template
 
 # url for themes
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
+load_figure_template('pulse')
 
 # import data 
 dr_data = pd.read_csv('data/drag.csv', index_col=0)
@@ -21,10 +23,10 @@ dob = (
     .pivot(index = ['season', 'rank', 'contestant'], columns = 'outcome', values = 'count')
     .reset_index()
     .fillna(0)
-)
+)[['season', 'rank', 'contestant', 'WIN', 'HIGH', 'SAFE', 'LOW', 'BTM']]
 dob['dob'] = 2 * dob['WIN'] + dob['HIGH'] - dob['LOW'] - 2 * dob['BTM']
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MINTY, dbc_css])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.PULSE, dbc_css])
 
 app.layout = dbc.Container([
     html.H1('Drag Race Visualizer'),
@@ -56,10 +58,10 @@ app.layout = dbc.Container([
             dbc.Button(
                 "Show Info",
                 id="collapse-button",
-                className="mb-3",
-                color="primary",
+                color="info",
                 size="sm"
             ),
+            html.Br(),
             html.Br(),
             html.Label('Seasons:'),
             dcc.Dropdown(
@@ -77,14 +79,34 @@ app.layout = dbc.Container([
                 multi=True,
                 placeholder='Select one or more queens'
             )
-        ], md = 3),
+        ], md = 2),
         dbc.Col([
+            html.H3('Scores'),
             dcc.Graph(
                 id = 'performance'
+            ),
+        ], md = 5),
+        dbc.Col([
+            html.H3('Performance Summary'),
+            dash_table.DataTable(
+                id='table', 
+                columns=[{"name": col, "id": col} for col in dob.columns],
+                sort_action="native",
+                page_size=15,
+                fixed_rows={'headers': True},
+                style_cell={'textAlign': 'left'},
+                style_cell_conditional=[
+                    {'if': {'column_id': 'contestant'},
+                    'width': '30%'},
+                    {'if': {'column_id': ['season']},
+                    'width': '10%'},
+                    {'if': {'column_id': ['dob', 'rank', 'BTM', 'HIGH', 'LOW', 'SAFE', 'WIN']},
+                    'width': '7%'},
+                ]
             )
-        ], md = 9)
+        ], md = 5)
     ])
-], className = "dbc")
+], className = "dbc", fluid = True)
 
 @app.callback(
     Output("helptext", "is_open"),
@@ -112,7 +134,7 @@ def update_queens(season, queens):
     if season is not None and len(season) != 0:
         seasonal = dr_data.query("season in @season")
         options = sorted(seasonal['contestant'].unique())
-        values = seasonal.sort_values('rank')['contestant'].unique()[:2]
+        values = None
     else:
         options = sorted(dr_data['contestant'].unique())
         if queens is None or len(queens) == 0:
@@ -123,6 +145,7 @@ def update_queens(season, queens):
 
 @app.callback(
     Output('performance', 'figure'),
+    Output('table', 'data'),
     Input('season', 'value'),
     Input('queen', 'value')
 )
@@ -139,9 +162,9 @@ def plot_performance(season, queen):
         x = 'dob',
         y = 'contestant',
         color = 'dob',
+        text = 'dob',
         color_continuous_scale='Plotly3',
         range_color=[-10, 15],
-        title = 'Contestant Performance Scores',
         labels = {
             'dob': 'Dusted or Busted Score',
             'season': 'Season',
@@ -155,11 +178,13 @@ def plot_performance(season, queen):
         hover_name = 'contestant',
         hover_data = {'contestant': False, 'season': True, 'rank': True, 'WIN': True, 'HIGH': True, 'LOW': True, 'BTM': True})
     
+    fig.update_traces(textangle=0, textposition="outside")
+
     fig.update_layout(
         coloraxis_showscale = False,
         margin=dict(l=10, r=10, t=30, b=0)
     )
-    return fig
+    return fig, filtered.sort_values(['rank', 'dob'], ascending = [True, False]).to_dict('records')
     
 
 if __name__ == '__main__':
